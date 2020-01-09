726714430324e50470d324f3b52d2a55be446a4d
ben <benjaminfkile@gmail.com>
2020-01-08 21:42:24 -0700

    mentor 1/8/2019

---
+++ migrations/001.do.populate-noteful.sql
@@ -0,0 +1,6 @@
+insert into folders (id, name)
+values 
+('b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1', 'Important'),
+('b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1', 'Super'),
+('b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1', 'Spangley')
+
